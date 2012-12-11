a92b4387c3f2e86a6104297e03c8ae36bcad248a
Tim Tkachenko <ateamdev@gmail.com>
2012-12-11 23:49:48 +0200

    setup almost done2

---
+++ src/NXC/SafeBundle/Entity/Message.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace NXC\SafeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Message
+ *
+ * @ORM\Table(name="message")
+ * @ORM\Entity(repositoryClass="NXC\SafeBundle\Entity\MessageRepository")
+ */
+class Message
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="body", type="blob")
+     */
+    private $body;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="modefied", type="datetime")
+     */
+    private $modefied;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set body
+     *
+     * @param string $body
+     * @return Message
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * Get body
+     *
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return Message
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set modefied
+     *
+     * @param \DateTime $modefied
+     * @return Message
+     */
+    public function setModefied($modefied)
+    {
+        $this->modefied = $modefied;
+
+        return $this;
+    }
+
+    /**
+     * Get modefied
+     *
+     * @return \DateTime
+     */
+    public function getModefied()
+    {
+        return $this->modefied;
+    }
+}
